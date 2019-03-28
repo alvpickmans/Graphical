f1c0d7fba055cc3e5790df4d0bca21ca2d390932
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-03-28 09:28:46 +0000

    convexgraph class

---
+++ Graphical/src/Graphs/ConvexGraph.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphical.Graphs
+{
+    public class ConvexGraph : Graph
+    {
+    }
+}
