e892a5299c3489e9b29eb2f2d3ad4a873afe216e
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-04-16 17:32:11 +0100

    bringing back `BuildPolygons`

--- Graphical/src/Graphs/Graph.cs
+++ Graphical/src/Graphs/Graph.cs
@@ -126,11 +126,6 @@ namespace Graphical.Graphs
             }
         }
 
-        //public List<Vertex> GetAdjecentVertices(Vertex v)
-        //{
-        //    return _vertexEdgesDict[v].Select(edge => edge.GetVertexPair(v)).ToList();
-        //}
-
         /// <summary>
         /// Add edge to the analisys graph
         /// </summary>
@@ -160,6 +155,85 @@ namespace Graphical.Graphs
             if (!Edges.Contains(edge)) { Edges.Add(edge); }
         }
 
+        /// <summary>
+        /// Computes Edges and creates _polygonsDict from those connected by Vertices.
+        /// </summary>
+        public void BuildPolygons()
+        {
+            var computedVertices = new List<Vertex>();
+
+            foreach (Vertex v in Vertices)
+            {
+                // If already belongs to a polygon or is not a polygon vertex or already computed
+                if (computedVertices.Contains(v) || _vertexEdgesDict[v].Count > 2) { continue; }
+
+                computedVertices.Add(v);
+                Polygon polygon = new Polygon(false);
+
+                polygon.AddVertex(v);
+                foreach (Edge edge in GetVertexEdges(v))
+                {
+                    Edge currentEdge = edge;
+                    Vertex currentVertex = edge.GetVertexPair(v);
+                    while (!polygon.vertices.Contains(currentVertex) || !computedVertices.Contains(currentVertex))
+                    {
+                        polygon.AddVertex(currentVertex);
+                        polygon.edges.Add(currentEdge);
+
+                        var connectedEdges = _vertexEdgesDict[currentVertex];
+                        //It is extreme vertex, polygon not closed
+                        if (connectedEdges.Count < 2)
+                        {
+                            break;
+                        }
+                        // If just two Edges, select the one that is not current nextEdge
+                        else if (connectedEdges.Count == 2)
+                        {
+                            currentEdge = connectedEdges[0].Equals(currentEdge) ? connectedEdges[1] : connectedEdges[0];
+                        }
+                        // If 4, is self intersection
+                        else if (connectedEdges.Count == 4)
+                        {
+                            var edgesWithVertexAlreadyInPolygon = connectedEdges
+                                .Where(e => !e.Equals(currentEdge) && polygon.Vertices.Contains(e.GetVertexPair(currentVertex)))
+                                .ToList();
+                            //If any of them connects to a vertex already on the current polygon,
+                            // If only one, set as current and it will close the polygon on the next iteration
+                            if (edgesWithVertexAlreadyInPolygon.Count == 1)
+                            {
+                                currentEdge = edgesWithVertexAlreadyInPolygon.First();
+                            }
+                            // If two, it means that is a intersection with two previous Edges computed,
+                            // so set the next to the one that is not parallel to current
+                            else if (edgesWithVertexAlreadyInPolygon.Count == 2)
+                            {
+                                currentEdge = edgesWithVertexAlreadyInPolygon[0].Direction.IsParallelTo(currentEdge.Direction) ?
+                                    edgesWithVertexAlreadyInPolygon[1] :
+                                    edgesWithVertexAlreadyInPolygon[0];
+                            }
+                            // More than two, none on the current polygon so select one of those not yet computed
+                            else
+                            {
+                                polygon.edges.Reverse();
+                                polygon.Vertices.Reverse();
+                                break; //it will go the other way around
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("WARNING. Something unexepected happend with the _polygonsDict...");
+                        }
+                        computedVertices.Add(currentVertex);
+                        currentVertex = currentEdge.GetVertexPair(currentVertex);
+                    }
+                    if (!polygon.edges.Last().Equals(currentEdge))
+                    {
+                        polygon.edges.Add(currentEdge);
+                    }
+                }
+                this._polygonsDict.Add(polygon.Id, polygon);
+            }
+        }
         #endregion
 
         #region Override Methods
